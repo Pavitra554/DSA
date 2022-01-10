5d106bedcaf4141f173a67df2c0a213390dca831
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-01-10 09:52:48 +0530

    Adding code for Prim's Minimum spanning tree

---
+++ Graph_Theory/Prims_MST.java
@@ -0,0 +1,120 @@
+package DATA_STRUCTURE.Graph_Theory;
+
+import java.util.*;
+public class Prims_MST {
+    //Matrix representation of graph
+    static int [][]graph;
+    //Array for storing MST 
+    static int [] parent;
+
+    //Function for adding edges
+    static void addEdge(int source,int destination,int weight){
+        graph[source][destination] = weight;
+        graph[destination][source] = weight;
+    }
+
+    //Function for finding minimum key 
+    static int MinimumKey(int key[],boolean included[]){
+    
+        int min  = Integer.MAX_VALUE;
+        int minINDEX = -1;
+        for (int i = 0; i < graph.length; i++) {
+            if(included[i] == false && key[i]<min){
+                min = key[i];
+                minINDEX = i;
+            }
+        }
+        return minINDEX;
+    }
+
+    //Function for applying Prim's algorithm
+    static void Prims(){
+        //initilizing parent array
+        parent  = new int[graph.length];
+
+        //Array for storing keys
+        int [] key = new int[graph.length];
+
+        //Array for checking that edge is included in MST or not
+        boolean [] included = new boolean[graph.length];
+
+        //Initializing keys with infinite(max int value) and included with false
+        for (int i = 0; i < graph.length; i++) {
+            key[i] = Integer.MAX_VALUE;
+            included[i] = false;
+        }
+
+        //for the first vertex key value is 0 and parent is -1
+        key[0] = 0;
+        parent[0] = -1;
+
+
+        for (int i = 0; i < graph.length; i++) {
+            //finding vertex with minimum key value
+            int u = MinimumKey(key, included);
+
+            //visiting the vertex
+            included[u] = true;
+
+
+            for (int j = 0; j < graph.length; j++) {
+                if(graph[u][j] != 0 && included[j] ==false && graph[u][j]<key[j]){
+                    
+                    parent[j] = u;
+                    key[j] = graph[u][j];
+                }
+            }
+        }
+    }
+
+    //Printing the minimum spanning tree
+   static void printMST(){
+       
+        int totalWeigth = 0;
+        System.out.println("Edge \tWeight");
+        for (int i = 1; i < graph.length; i++){
+            System.out.println(parent[i] + " - " + i + "\t" + graph[i][parent[i]]);
+            totalWeigth += graph[i][parent[i]];
+        }
+        //printing totale weight of minimum spanning tree 
+        System.out.println("Total weight : "+totalWeigth);
+    }
+
+    //Printing matrix of graph
+    static void PrintAdjecencyMatrix(){
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph.length; j++) {
+                System.out.print(graph[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of vertices : ");
+        int vertex = s.nextInt();
+        System.out.println("Enter the number of edges : ");
+        int edge = s.nextInt();
+
+        //Creating v * v size of graph
+        graph = new int[vertex][vertex];
+
+        for (int i = 0; i < edge; i++) {
+            System.out.println("Enter source, destination and weight : ");
+            int src = s.nextInt();
+            int des = s.nextInt();
+            int wgt = s.nextInt();
+            
+            addEdge(src, des, wgt);
+        }
+        //Funtion for printing adjecency matrix
+        PrintAdjecencyMatrix();
+        //Function for prims algorithm
+        Prims();
+        //Printing minimum spanning tree
+        printMST();
+
+        s.close();
+
+    }
+}
