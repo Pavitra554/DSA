c78e9067676c0170d8d2915f0a0e58a31d1a8984
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-03-20 00:09:54 +0530

    collection --> Set

---
+++ Java_CollectionFramework/Set_HashSet.java
@@ -0,0 +1,28 @@
+package DATA_STRUCTURE.Java_CollectionFramework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Set_HashSet {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<Integer>();
+        // This doesn't maintain an order
+        // This will contain only unique element (no duplicate elements).
+        set.add(45);
+        set.add(4);
+        set.add(41);
+        set.add(1);
+        set.add(12);
+        set.add(18);
+        set.add(18);
+        set.add(18);// this will add 18 only one time
+        System.out.println(set);
+
+        set.remove(18);// remove element 18
+        System.out.println(set);
+        System.out.println(set.contains(1));
+        System.out.println(set.isEmpty());
+        set.clear();
+        System.out.println(set);
+    }
+}
