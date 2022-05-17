40ba29d2623ce1b9372130a811e8bf6ab2eeddca
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-05-17 07:57:33 +0530

    Sort --> Count Sort

--- Sort/Counting_Sort.java
+++ Sort/Counting_Sort.java
@@ -3,21 +3,45 @@ package DATA_STRUCTURE.Sort;
 import java.util.*;
 
 public class Counting_Sort {
-    public static void main(String[] args) {
-        int arr[] = { 2, 6, 4, 1, 5, 8, 1, 4, 6, 1 };
-        int ans[] = new int[arr.length];
-        for (int i = 0; i < ans.length; i++) {
-            ans[arr[i]]++;
-        }
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(ans));
-
-        int k = 0;
-        for (int i = 0; i < ans.length; i++) {
-            for (int j = 0; j < ans[i]; j++) {
-                arr[k++] = i;
+    static int findMax(int arr[]) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
             }
         }
+        return max;
+    }
+
+    static void countSort(int arr[]) {
+        int count[] = new int[findMax(arr) + 1];
+        int sort[] = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        for (int i = 1; i < count.length; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Important Loop
+        System.out.println(Arrays.toString(count)); // Modified count array [0,3,7,8]
+
+        for (int i = 0; i < arr.length; i++) {
+            sort[count[arr[i]] - 1] = arr[i];
+            count[arr[i]]--;
+        }
+        for (int i = 0; i < sort.length; i++) {
+            arr[i] = sort[i];
+        }
+        System.out.println(Arrays.toString(count)); // Final count array [0,0,3,7]
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 1, 2, 3, 2, 2, 1, 1 };
+        countSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
